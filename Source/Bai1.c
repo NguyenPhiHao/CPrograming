4038001d9a7ae57bb00de5103ab719cd3072eb97
haonp1999@gmail.com <dovantu2789@gmail.com>
2023-06-16 15:22:03 +0700

    re-edit and synchronized file

---
+++ Source/Bai1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#define PHUC 100
+
+int main() {
+    int a;
+
+    a = PHUC;
+    
+    printf("Nhap a: ");
+    scanf("%i %i",   &a, &b );
+
+
+    return 0;
+}
+
+
+
+// in ra     "Phuc" 20 tuoi
